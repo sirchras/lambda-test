721b8b6cab25c9562259a7928ccbc8e5d1933e35
sirchras <cjlynch@hotmail.co.nz>
2022-01-29 17:09:20 +1300

    add logging
    cool decorators, unsure how logging will work in separate functions

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,21 @@
 import json
+import logging
+from functools import wraps
 
+def log_cloudwatch(fn):
+  logger = logging.getLogger()
+  logger.setLevel(logging.INFO)
+
+  @wraps(fn)
+  def wrapper(event, context):
+    logger.info(f'{context.function_name} - entry:{event}')
+    res = fn(event, context)
+    logger.info(f'{context.function_name} - result:{res}')
+    return res
+
+  return wrapper
+
+@log_cloudwatch
 def lambda_handler(event, context):
   # TODO implement
   return {
